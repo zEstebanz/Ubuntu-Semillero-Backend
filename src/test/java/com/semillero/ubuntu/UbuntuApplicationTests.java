df5e7d3ff4766b665294dd86229ec12a48f6bf46
Sem_Ubuntu003 <>
2024-02-16 13:57:42 +0000

    Initial commit

---
+++ src/test/java/com/semillero/ubuntu/UbuntuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semillero.ubuntu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbuntuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
