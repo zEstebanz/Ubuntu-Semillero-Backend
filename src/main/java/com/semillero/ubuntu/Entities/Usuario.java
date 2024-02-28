f8dac9bae07a6d0f23ad8b6a1ce2d5936b050265
Niklasss98 <nlucca98@gmail.com>
2024-02-28 15:59:37 -0300

    Rama Nico

---
+++ src/main/java/com/semillero/ubuntu/Entities/Usuario.java
@@ -0,0 +1,10 @@
+package com.semillero.ubuntu.Entities;
+
+public class Usuario extends BaseEntity {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private Boolean isDeleted;
+    //private Rol rol;
+
+}
