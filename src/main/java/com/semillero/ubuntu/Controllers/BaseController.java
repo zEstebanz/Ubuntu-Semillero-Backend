a333600f6490bd071f78546461563191b9017124
Niklasss98 <nlucca98@gmail.com>
2024-02-28 17:23:07 -0300

    Chau entidades base

--- src/main/java/com/semillero/ubuntu/Controllers/BaseController.java
+++
@@ -1,8 +0,0 @@
-package com.semillero.ubuntu.Controllers;
-
-import com.semillero.ubuntu.Entities.BaseEntity;
-
-import java.io.Serializable;
-
-public interface BaseController <E extends BaseEntity, ID extends Serializable>{
-}
--- src/main/java/com/semillero/ubuntu/Enums/EjemploEnum.java
+++
@@ -1,4 +0,0 @@
-package com.semillero.ubuntu.Enums;
-
-public enum EjemploEnum {
-}
