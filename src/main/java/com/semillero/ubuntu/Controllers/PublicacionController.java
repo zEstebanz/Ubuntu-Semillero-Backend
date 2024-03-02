efcd848e55cab6dfd71c6a137b7a04ebcc432f46
Niklasss98 <nlucca98@gmail.com>
2024-03-02 19:53:39 -0300

    Feature Publicaciones

--- src/main/java/com/semillero/ubuntu/Controllers/PublicacionController.java
+++ src/main/java/com/semillero/ubuntu/Controllers/PublicacionController.java
@@ -1,7 +1,25 @@
 package com.semillero.ubuntu.Controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.semillero.ubuntu.Entities.Publicacion;
+import com.semillero.ubuntu.Services.PublicacionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
+@RequestMapping(path = "publicaciones")
 public class PublicacionController {
+    @Autowired
+    private PublicacionService publicacionService;
+
+    @PostMapping("/create")
+    public ResponseEntity<?> crearPublicacion(@RequestBody Publicacion publicacion) {
+        try {
+
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{}")
+        }
+    }
 }
