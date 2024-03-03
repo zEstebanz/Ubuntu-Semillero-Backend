cebe946b1d53d0136a3b10fc854f5ff336a0fa19
vivi-ana <acostaviviana.a@gmail.com>
2024-03-03 16:49:53 -0300

    Add service, controller and mapper dependency

--- src/main/java/com/semillero/ubuntu/Controllers/RubroController.java
+++ src/main/java/com/semillero/ubuntu/Controllers/RubroController.java
@@ -1,2 +1,23 @@
-package com.semillero.ubuntu.Controllers;public class RubroController {
+package com.semillero.ubuntu.Controllers;
+
+import com.semillero.ubuntu.DTOs.RubroDTO;
+import com.semillero.ubuntu.Services.RubroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rubros")
+public class RubroController {
+    @Autowired
+    RubroService rubroService;
+    @GetMapping("/get-all")
+    public ResponseEntity<List<RubroDTO>> getAllRubros() {
+        List<RubroDTO> rubroList = rubroService.getAllRubros();
+        return ResponseEntity.ok(rubroList);
+    }
 }
