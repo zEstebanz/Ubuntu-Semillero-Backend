cdb7d1b535d7a86b76bd5465619d08d0f2e91342
vivi-ana <acostaviviana.a@gmail.com>
2024-03-03 16:52:02 -0300

    Insert initial data for the database using a seeder

--- src/main/java/com/semillero/ubuntu/DTOs/RubroDTO.java
+++ src/main/java/com/semillero/ubuntu/DTOs/RubroDTO.java
@@ -1,2 +1,13 @@
-package com.semillero.ubuntu.DTOs;public class RubroDTO {
+package com.semillero.ubuntu.DTOs;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class RubroDTO {
+    private Long id;
+    private String nombre;
 }
--- src/main/java/com/semillero/ubuntu/DataSeed/RubroDataLoader.java
+++ src/main/java/com/semillero/ubuntu/DataSeed/RubroDataLoader.java
@@ -1,2 +1,30 @@
-package com.semillero.ubuntu.DataSeed;public class RubroDataLoader {
+package com.semillero.ubuntu.DataSeed;
+
+import com.semillero.ubuntu.Entities.Rubro;
+import com.semillero.ubuntu.Repositories.RubroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RubroDataLoader implements CommandLineRunner {
+    @Autowired
+    RubroRepository rubroRepository;
+    @Override
+    public void run(String... args) throws Exception {
+        loadRubroData();
+    }
+
+    private void loadRubroData() {
+        if (rubroRepository.count() == 0) {
+            Rubro rubro1 = new Rubro(1L, "Economía social/ Desarrollo local/ Inclusión financiera");
+            Rubro rubro2 = new Rubro(2L, "Agroecología/ Orgánicos/ Alimentación saludables");
+            Rubro rubro3 = new Rubro(3L, "Conservación/ Regeneración/ Servicios ecosistémicos");
+            Rubro rubro4 = new Rubro(4L, "Empresas/ Organismos de impacto/ Economía circular");
+            rubroRepository.save(rubro1);
+            rubroRepository.save(rubro2);
+            rubroRepository.save(rubro3);
+            rubroRepository.save(rubro4);
+        }
+    }
 }
