9b1bb03fa4b8d2f2ccff2cd30b510f4c6cce3ce0
Niklasss98 <nlucca98@gmail.com>
2024-03-15 12:14:19 -0300

    Comentarios

--- src/main/java/com/semillero/ubuntu/Services/impl/PublicacionServiceImpl.java
+++ src/main/java/com/semillero/ubuntu/Services/impl/PublicacionServiceImpl.java
@@ -117,7 +117,9 @@ public class PublicacionServiceImpl implements PublicacionService {
      publicación
      <p>
      (Ver como modelar la lógica de esta funcionalidad, si creando otro endpoint o verificando el rol del usuario
-     logueado con el token)
+     logueado con el token, charlar con el front sobre como implementan la lógica. Ver si aumenta la vista una vez por
+     visitante o si aumenta sin restricciones, cuando selecciona 'ver más' o si cuando se renderiza. Si es usando el botón
+     que no sume dos veces, o quizás sí)
      <p>
      Rol: VISITANTE (MUY IMPORTANTE)
      **/
