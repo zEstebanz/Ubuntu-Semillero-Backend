f205b0d07643838eef618ffe32cf250dd35342c9
Niklasss98 <nlucca98@gmail.com>
2024-03-20 11:48:04 -0300

    CrossOrigin

--- src/main/java/com/semillero/ubuntu/Controllers/RubroController.java
+++ src/main/java/com/semillero/ubuntu/Controllers/RubroController.java
@@ -4,12 +4,14 @@ import com.semillero.ubuntu.DTOs.RubroDTO;
 import com.semillero.ubuntu.Services.RubroService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin("http://localhost:5173")
 @RestController
 @RequestMapping("/rubros")
 @RequiredArgsConstructor
