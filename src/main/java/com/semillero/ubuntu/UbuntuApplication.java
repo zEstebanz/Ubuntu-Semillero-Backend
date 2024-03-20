926b11c22e0ea9a768caea5990e548e92f65109b
Niklasss98 <nlucca98@gmail.com>
2024-03-20 12:04:34 -0300

    CrossOrigin

--- src/main/java/com/semillero/ubuntu/UbuntuApplication.java
+++ src/main/java/com/semillero/ubuntu/UbuntuApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-//Colocar la variable de entorno en el lugar de la URL
+//Ver como colocar la anotacion en SecurityConfig con una variable de entorno
 @CrossOrigin(origins = "http://localhost:5173")
 @SpringBootApplication
 public class UbuntuApplication {
